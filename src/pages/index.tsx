5c32ca25b47910586e30bc2a3fd992ce4198f41c
frontend developer <vn.developer@commude.co.jp>
2024-12-17 21:10:46 +0700

    new add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -115,7 +115,7 @@ export default function Home() {
                             width={16}
                             height={16}
                         />
-                        Go to nextjs.org →
+                        Go to nextjs.org → enter
                     </a>
                 </footer>
             </div>
